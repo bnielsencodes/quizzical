55a7275eb8bee4444faaf47eb31c8bb3892c13fa
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-29 23:36:58 -0800

    Create startGame() function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 export default function App() {
   const [gameStarted, setGameStarted] = useState(false);
   const [triviaData, setTriviaData] = useState([]);
+
+  function startGame() {
+    setGameStarted(true);
+  }
+
   // fetch data from Open TDB API
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
