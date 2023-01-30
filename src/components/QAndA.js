20b6b44836be2a9ca52fd9f2af6265f1ef0df06c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-29 23:40:33 -0800

    Import html-entities

--- src/components/QAndA.js
+++ src/components/QAndA.js
@@ -1,4 +1,10 @@
+import { decode } from "html-entities";
 export default function QAndA(props) {
+  /* Decode html entities to display characters. */
+  function formatData(data) {
+    return decode(data);
+  }
+
   return (
     <div className="q-and-a">
     </div>
