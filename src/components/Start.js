40fc5eb1cbc526d1cffadb523496bebd28be1d95
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-29 23:31:55 -0800

    Add blob images to background

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,6 +1,15 @@
+import blobBlueStart from "../assets/blob-blue-start.png";
+import blobYellowStart from "../assets/blob-yellow-start.png";
+
 export default function Start(props) {
   return (
     <div className="start">
+      <img className="blob-blue" src={blobBlueStart} alt="blue blob shape" />
+      <img
+        className="blob-yellow"
+        src={blobYellowStart}
+        alt="yellow blob shape"
+      />
       <div className="start--inner-container">
         <h1 className="start--title">Quizzical</h1>
         <h4 className="start--subtitle">How well do you know your trivia?</h4>
