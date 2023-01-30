3eabdd2ecaeb102b027e5babaedc1970ca3cef9d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-29 23:39:06 -0800

    Add onClick to start--btn

--- src/components/Start.js
+++ src/components/Start.js
@@ -13,7 +13,9 @@ export default function Start(props) {
       <div className="start--inner-container">
         <h1 className="start--title">Quizzical</h1>
         <h4 className="start--subtitle">How well do you know your trivia?</h4>
-        <button className="start--btn">Start Quiz</button>
+        <button className="start--btn" onClick={props.startGame}>
+          Start Quiz
+        </button>
       </div>
     </div>
   );
