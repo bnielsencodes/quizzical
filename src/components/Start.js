d512f5b447d7a0e823117fb6a36f8d8c8cf101f3
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-29 23:30:41 -0800

    Create Start.js

---
+++ src/components/Start.js
@@ -0,0 +1,10 @@
+export default function Start(props) {
+  return (
+    <div className="start">
+      <div className="start--inner-container">
+        <h1 className="start--title">Quizzical</h1>
+        <h4 className="start--subtitle">How well do you know your trivia?</h4>
+      </div>
+    </div>
+  );
+}
