813b38fe502cf4bed8c564eb680af4782965d892
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-29 23:33:13 -0800

    Add blob images to background

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,11 @@
+import blobBlue from "../assets/blob-blue.png";
+import blobYellow from "../assets/blob-yellow.png";
+
 export default function Quiz(props) {
   return (
     <div className="quiz">
+      <img className="blob-blue" src={blobBlue} alt="blue blob shape" />
+      <img className="blob-yellow" src={blobYellow} alt="yellow blob shape" />
       <button className="quiz--btn">Check Answers</button>
     </div>
   );
