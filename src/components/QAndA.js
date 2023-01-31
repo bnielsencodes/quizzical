30ca1ded5d27444af39709c1ba4c4e8ce1ac6fdd
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-30 23:16:51 -0800

    Decode questions data

--- src/components/QAndA.js
+++ src/components/QAndA.js
@@ -1,5 +1,6 @@
 // import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
+import { decode as atob } from "base-64";
 
 export default function QAndA(props) {
   let answers = props.item.answers;
@@ -38,6 +39,7 @@ export default function QAndA(props) {
 
   return (
     <div className="q-and-a">
+      <p className="question">{atob(props.item.question)}</p>
       <div className="answers-container">{answerElements}</div>
     </div>
   );
