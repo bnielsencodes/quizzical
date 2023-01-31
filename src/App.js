03b0bcfed9a72f86eb16d00b0c9ffca98df38acd
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-30 23:19:28 -0800

    Fetch from API, create questionsArr, setQuestions

--- src/App.js
+++ src/App.js
@@ -18,7 +18,31 @@ export default function App() {
     return arr.sort(() => Math.random() - 0.5);
   }
 
+  // fetch from API, create questionsArr, setQuestions
   useEffect(() => {
+    async function getQuestions() {
+      const res = await fetch(
+        "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple&encode=base64"
+      );
+      const data = await res.json();
+      let questionsArr = [];
+      data.results.forEach((item) => {
+        questionsArr.push({
+          id: nanoid(),
+          question: item.question,
+          answers: shuffleArray([
+            ...item.incorrect_answers,
+            item.correct_answer,
+          ]),
+          correct: item.correct_answer,
+          selected: null,
+          checked: false,
+        });
+      });
+      setQuestions(questionsArr);
+    }
+    getQuestions();
+  }, [newGame]);
 
 
   // map through API data fetched to create QAndA components
