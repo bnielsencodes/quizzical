afb27095ef08bba35ccc102bef58ead100d41f30
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-30 23:16:33 -0800

    Map through answers variable

--- src/components/QAndA.js
+++ src/components/QAndA.js
@@ -1,5 +1,8 @@
+// import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 export default function QAndA(props) {
+  let answers = props.item.answers;
 
   function handleSelectAnswer(answer) {
     if (props.item.checked) {
@@ -8,13 +11,34 @@ export default function QAndA(props) {
     props.selectAnswer(props.id, answer);
   }
 
+  const answerElements = answers.map((answer) => {
+    let id = null;
+    if (props.item.checked) {
+      if (props.item.correct === answer) {
+        id = "correct";
+      } else if (props.item.selected === answer) {
+        id = "incorrect";
+      } else {
+        id = "not-selected";
+      }
+    }
     return (
+      <button
+        key={nanoid()}
+        id={id}
+        className={
+          answer === props.item.selected ? "answer selected" : "answer"
+        }
+        onClick={() => handleSelectAnswer(answer)}
+      >
+        {atob(answer)}
       </button>
     );
   });
 
   return (
     <div className="q-and-a">
+      <div className="answers-container">{answerElements}</div>
     </div>
   );
 }
