c6cbe2b41b14c88c827313677c98b5030bd1ba25
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-05 22:28:21 -0800

    Create GameOptions.js

---
+++ src/components/GameOptions.js
@@ -0,0 +1,12 @@
+export default function GameOptions(props) {
+  return (
+    <div className="game-options">
+      <div className="select-container">
+      </div>
+      <div className="select-container">
+      </div>
+      <div className="select-container">
+      </div>
+    </div>
+  );
+}
