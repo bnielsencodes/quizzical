83e504cfdb25c6180d442e1e03d75cd0bae737f4
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-05 22:31:41 -0800

    Create select elements

--- src/components/GameOptions.js
+++ src/components/GameOptions.js
@@ -5,16 +5,37 @@ export default function GameOptions(props) {
         <label className="select-label" htmlFor="category">
           Category:
         </label>
+        <select
+          name="category"
+          id="category"
+          className="select"
+          value={props.gameOptions.category}
+        >
+        </select>
       </div>
       <div className="select-container">
         <label className="select-label" htmlFor="difficulty">
           Difficulty:
         </label>
+        <select
+          name="difficulty"
+          id="difficulty"
+          className="select"
+          value={props.gameOptions.difficulty}
+        >
+        </select>
       </div>
       <div className="select-container">
         <label className="select-label" htmlFor="type">
           Type of questions:
         </label>
+        <select
+          name="type"
+          id="type"
+          className="select"
+          value={props.gameOptions.type}
+        >
+        </select>
       </div>
     </div>
   );
