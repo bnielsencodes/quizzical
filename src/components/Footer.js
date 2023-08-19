876c15bc769661d1f43e4efff2f25d01205122fa
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-08-18 22:51:45 -0700

    Add underlineAnimation to footer signature

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ export default function Footer() {
   return (
     <p className="footer">
       Made by{" "}
-      <a href="https://bnielsen.dev" target="_blank" rel="noreferrer">
+      <a
+        className="underlineAnimation"
+        href="https://bnielsen.dev"
+        target="_blank"
+        rel="noreferrer"
+      >
         Brandon Nielsen
       </a>
     </p>
